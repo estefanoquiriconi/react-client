811aa820fff41771795f411157ca4465f5bd9e64
estefanoquiriconi <estefanoquiriconi@gmail.com>
2025-03-30 22:44:08 -0300

    Add axios dependency and implement product addition service with validation schema

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,7 +1,28 @@
+import { safeParse } from 'valibot';
+import { DraftProductSchema } from '../types';
+import axios from 'axios';
+
 interface ProductData {
   [k: string]: FormDataEntryValue;
 }
 
 export async function addProduct(data: ProductData) {
-  console.log(data);
+  try {
+    const result = safeParse(DraftProductSchema, {
+      name: data.name,
+      price: +data.price,
+    });
+    if (result.success) {
+      const url = `${import.meta.env.VITE_API_URL}/api/products`;
+      const { data } = await axios.post(url, {
+        name: result.output.name,
+        price: result.output.price,
+      });
+      console.log(data);
+    } else {
+      throw new Error('Datos no válidos');
+    }
+  } catch (err) {
+    console.error(err);
+  }
 }
