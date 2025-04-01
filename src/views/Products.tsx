cc03d20d6612403b871ecd561dc939d97146ed2f
estefanoquiriconi <estefanoquiriconi@gmail.com>
2025-03-31 23:57:39 -0300

    refactor: improve Products layout & responsiveness, adjust heading size, add table overflow

--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -6,8 +6,8 @@ export default function Products() {
   const products = useLoaderData() as Product[];
   return (
     <>
-      <div className='flex justify-between'>
-        <h2 className='text-4xl font-black text-slate-500'>Productos</h2>
+      <div className='flex flex-col md:flex-row justify-between items-center'>
+        <h2 className='text-3xl md:text-4xl font-black text-slate-500 mb-4 md:mb-0'>Productos</h2>
         <Link
           to={'productos/nuevo'}
           className='rounded bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500'>
@@ -15,24 +15,26 @@ export default function Products() {
         </Link>
       </div>
       <div className='p-2'>
-        <table className='w-full mt-5 table-auto'>
-          <thead className='bg-slate-800 text-white'>
-            <tr>
-              <th className='p-2'>Producto</th>
-              <th className='p-2'>Precio</th>
-              <th className='p-2'>Disponibilidad</th>
-              <th className='p-2'>Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => (
-              <ProductDetails
-                key={product.id}
-                product={product}
-              />
-            ))}
-          </tbody>
-        </table>
+        <div className='overflow-x-auto'>
+          <table className='min-w-full mt-5 table-auto'>
+            <thead className='bg-slate-800 text-white'>
+              <tr>
+                <th className='p-2 text-left'>Producto</th>
+                <th className='p-2 text-left'>Precio</th>
+                <th className='p-2 text-left'>Disponibilidad</th>
+                <th className='p-2 text-left'>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <ProductDetails
+                  key={product.id}
+                  product={product}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
